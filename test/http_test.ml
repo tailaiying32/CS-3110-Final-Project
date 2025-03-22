3899b23aeb597cc43bf83dd1f1e629800982ca55
Deniz Sanchez <ds2472@cornell.edu>
2025-03-22 15:02:15 -0400

    Set up a folder structure in lib. Moved response compilation unit into http folder.

--- test/http_test.ml
+++ test/http_test.ml
@@ -1,5 +1,7 @@
 open OUnit2
 open Final_project
 
-let tests = "Http test suite" >::: []
-let _ = run_test_tt_main tests
+let requests_tests = "http test requests interface" >::: []
+let _ = run_test_tt_main requests_tests
+let response_tests = "http test response interface" >::: []
+let _ = run_test_tt_main response_tests
