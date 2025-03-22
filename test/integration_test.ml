37901492526a6775f1ecbfa3b0c639c2de368228
Deniz Sanchez <ds2472@cornell.edu>
2025-03-22 13:46:52 -0400

    Added a structired test suite for HTTP, routing, server and Integration system. Configured dune file.

---
+++ test/integration_test.ml
@@ -0,0 +1,5 @@
+open OUnit2
+open Final_project
+
+let tests = "Integration system test suite" >::: []
+let _ = run_test_tt_main tests
---
+++ test/router_test.ml
@@ -0,0 +1,5 @@
+open OUnit2
+open Final_project
+
+let tests = "Routing system test suite" >::: []
+let _ = run_test_tt_main tests
