f953f63cab72ae6e97021e6edbe76d3a5fdfd255
Andrew C Park <a_park21@outlook.com>
2025-03-23 16:00:46 -0400

    Revised type rep of JSON compilation unit
    - Enabled the use of nested dictionaries

--- lib/json.ml
+++ lib/json.ml
@@ -1,11 +1,27 @@
-type elt =
-  | Simple of string * string
-  | Nested of string * elt
+type value =
+  | Dict of (string * value) list
+  | Entry of string
 
-type t = elt list
+type t = (string * value) list
 
-let key_of_elt = function
-  | Simple (f, _) -> f
-  | Nested (f, _) -> f
+exception EmptyJson
 
-let keys = List.map key_of_elt
+let keys (json : t) = List.map fst json
+let values (json : t) = List.map snd json
+let pairs (json : t) = json
+
+let rec add (json : t) key value : t =
+  match json with
+  | [] -> [ (key, value) ]
+  | (k, v) :: t ->
+      if k = key then (key, value) :: t else (k, v) :: add t key value
+
+let rec del (json : t) key : t =
+  match json with
+  | [] -> []
+  | (k, v) :: t -> if k = key then t else (k, v) :: del t key
+
+let rec lookup (json : t) key =
+  match json with
+  | [] -> raise EmptyJson
+  | (k, v) :: t -> if k = key then v else lookup t key
--- lib/json.mli
+++ lib/json.mli
@@ -1,3 +1,29 @@
+(** [value] is the type representing the valid values of a json object *)
+type value =
+  | Dict of (string * value) list
+  | Entry of string
+
 type t
+(** [t] is the type representing json objects. *)
+
+exception EmptyJson
+(** [EmptyJson] is the exception to be raised when attempting to index and empty
+    json object. *)
 
 val keys : t -> string list
+(** [keys json] is a list of the keys of [json]. *)
+
+val values : t -> value list
+(** [values json] is a list of the values of [json]. *)
+
+val pairs : t -> (string * value) list
+(** [pairs json] is the list representation of [json]. *)
+
+val add : t -> string -> value -> t
+(** [add json key value] is the [json] with the new [(key, value)] added. *)
+
+val del : t -> string -> t
+(** [del json key] is the [json] with [key] removed. *)
+
+val lookup : t -> string -> value
+(** [lookup json key] is the value in [json] paired with [key]. *)
