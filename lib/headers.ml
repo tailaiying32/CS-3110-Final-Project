29fff8909c1e4c6b9bbe0503c11a0dc26c8cb798
Lucas Doell <lmd248@cornell.edu>
2025-05-15 19:33:08 -0400

    Fix response handling

--- lib/headers.ml
+++ lib/headers.ml
@@ -1,10 +1,14 @@
 type t = {
   host : string;
   content_type : string;
+  content_length : int;
 }
 
-let t_of host content_type = { host; content_type }
+let t_of host content_type = { host; content_type; content_length = 0 }
+
+let t_of_with_length host content_type content_length =
+  { host; content_type; content_length }
 
 let string_of_t headers =
-  Printf.sprintf "Content-Type: %s\r\nHost: %s\r\nContent-Length: 0\r\n"
-    headers.content_type headers.host
+  Printf.sprintf "Content-Type: %s\r\nHost: %s\r\nContent-Length: %d\r\n"
+    headers.content_type headers.host headers.content_length
