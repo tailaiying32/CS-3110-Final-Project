1f64d0df1488931d3404d2c9aa0366701ab471e6
Lucas Doell <lmd248@cornell.edu>
2025-05-15 18:51:28 -0400

    Update router to handle query parameters
    - Modified the router type to accept query parameters in the function signature.
    - Updated the `add` function to reflect the new signature, allowing it to handle both request bodies and query parameters.
    - Enhanced the `get_response` function to extract and utilize query parameters from the request path.

--- lib/router.mli
+++ lib/router.mli
@@ -3,9 +3,13 @@ type t
 val init : unit -> t
 (** [init ()] is an empty router, with no path mappings. *)
 
-val add : t -> string -> string -> (Body.t -> Response.response) -> t
-(** [add router path fn] adds the [(path, fn)] pair to the router. *)
+val add :
+  t -> string -> string -> (Body.t -> Query_params.t -> Response.response) -> t
+(** [add router method path fn] adds the [(method, path, fn)] triple to the
+    router. The [fn] function now takes both a request body and query
+    parameters. *)
 
 val get_response : t -> string -> string -> Body.t -> Response.response
-(** [get_response router path arguments] is the [response] obtained when the
-    function corresponding to [path] is applied to [arguments]. *)
+(** [get_response router method path body] is the [response] obtained when the
+    function corresponding to [method] and [path] is applied to [body] and any
+    query parameters extracted from [path]. *)
